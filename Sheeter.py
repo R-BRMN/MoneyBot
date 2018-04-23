27312032baf6ffe94660086808604ddd6729af08
Ronnie Berman <ronnie.berman@MBP-172709.local>
2018-04-23 14:33:48 +0300

    Sheeter

--- Sheeter.py
+++ Sheeter.py
@@ -1,4 +1,11 @@
 import pygsheets
 
-def __init__(self):
-    self.gsheets_client = pygsheets.authorize()
+class Sheeter:
+    """Sheeter is responsible for managing the Spreadsheets on Google Drive.
+
+    Attributes:
+        gsheets_client: A pygsheets instance associated with the Google account
+                        whose client_id credentials are in the client_secret json.
+    """
+    def __init__(self):
+        self.gsheets_client = pygsheets.authorize()
