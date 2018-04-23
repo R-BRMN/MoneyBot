4e64115d45cdc1a6faca4b4c8714b9ba0d81c5d8
Ronnie Berman <ronnie.berman@MBP-172709.local>
2018-04-23 15:11:54 +0300

    For Evya's review

--- Sheeter.py
+++ Sheeter.py
@@ -1,7 +1,8 @@
 import pygsheets
+import datetime
 
 class Sheeter:
-    """Sheeter is responsible for managing the Spreadsheets on Google Drive.
+    """Sheeter is responsible for managing the spreadsheets on Google Drive.
 
     Attributes:
         gsheets_client: A pygsheets instance associated with the Google account
@@ -9,3 +10,19 @@ class Sheeter:
     """
     def __init__(self):
         self.gsheets_client = pygsheets.authorize()
+
+    def sheet_exists(self, name):
+        """True or False depending on whether the spreadsheet exists.
+        """
+        ssheet_list = self.gsheets_client.list_ssheets()
+        for ssheet in ssheets_list:
+            if ssheet["name"] == name:
+                return True
+        return False
+
+    def months_sheet_exists(self):
+        """True or False depending on whether current's month spreadsheet exists.
+        Spreadsheet name format is Month_YY.
+        """
+        curr_month = datetime.datetime.now().strftime("%B_%y")
+        return self.sheet_exists(curr_month)
