f18d24aae25f9c2557979d85252442393f6c5216
Ronnie Berman <ronnie.berman@MBP-172709.local>
2018-04-24 17:29:52 +0300

    gfdg

--- Tester.py
+++ Tester.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     print (Sheeter)
     Sheeter.initialize_gsheets()
     print (Sheeter.get_month_sheet())
+    Sheeter.create_sheet("test1234", first_worksheet="this_worked!")
